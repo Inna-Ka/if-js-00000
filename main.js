a937b5022098edbd6f68b7b29cec22753d3c04e6
Inna-Ka <kasperovicz.inna@gmail.com>
2021-02-04 16:12:04 +0300

    work with primitives

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 let user = "John Doe"
-student = "Inna"
-user=student
-// user Присвоилось имя Inna
 console.log(user)
+student = "Inna"
 console.log(student)
+user=student
+// user присвоилось имя Inna
+console.log(user)
+
+
+let test = 1;
+test = test + 1;
+console.log (test)
+test = test +"1"
+// к результату "2" допишется цифра 1 и в итоге будет "21"
+console.log (test)
+test -=1;
+// от 21 отнимется 1 и результат будет 20
+console.log (test)
+if (test>10){
+    test= true;
+} else{
+    test=  false;
+ }
+ // на консоль выведтся "true"
+    console.log (test)
