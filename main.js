0d3229b09001e758a2946c29f746e02e4b4959e2
Inna-Ka <kasperovicz.inna@gmail.com>
2021-02-04 19:08:51 +0300

    work with arrays

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(user)
 student = "Inna"
 console.log(student)
 user=student
-// user присвоилось имя Inna
+// user became the name Inna
 console.log(user)
 
 
@@ -11,15 +11,36 @@ let test = 1;
 test = test + 1;
 console.log (test)
 test = test +"1"
-// к результату "2" допишется цифра 1 и в итоге будет "21"
+// to result "2" will write number 1 and as a result "21"
 console.log (test)
 test -=1;
-// от 21 отнимется 1 и результат будет 20
+// from 21 subtract 1 and the result will be 20
 console.log (test)
 if (test>10){
     test= true;
 } else{
     test=  false;
  }
- // на консоль выведтся "true"
+ // console will show "true"
     console.log (test)
+
+let result = 1;
+let array = [2, 3, 5, 8];
+for (let i = 0; i < array.length; i++) 
+{result = result * array[i];}
+console.log (result);
+
+let arr = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i=0; i < arr.length; i++) {
+    if (arr[i] > 5 && arr[i] <10 ) { 
+    console.log (arr[i]);}
+    }
+
+let array_next = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let x=0; x < array_next.length; x++) {
+    if (array_next[x] % 2 == 0 ) { 
+    console.log (array_next[x]);}
+    }
+
+
+
